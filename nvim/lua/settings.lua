1f7c30156e06718bd018b254d70303ae196f66b3
lin.song <lin.song@rakuten.com>
2021-12-08 18:01:33 +0900

    Update

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -57,3 +57,19 @@ function! ToggleWindowSize()
     endif
 endfunction
 ]])                                                         --- hide vertical bar
+
+-- diagnostic
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+vim.diagnostic.config({
+  virtual_text = false,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = false,
+})
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
