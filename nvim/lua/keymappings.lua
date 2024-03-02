7227314e0c6259090ea41e8eb41684a9149b7816
Lin, Song | Jarod | AIPD <lin.song@rakuten.com>
2024-03-02 14:06:17 +0900

    Update

--- nvim/lua/keymappings.lua
+++ nvim/lua/keymappings.lua
@@ -25,7 +25,7 @@ vim.api.nvim_set_keymap('n', '<Leader>o', '<CMD>Telescope find_files<CR>', opts_
 vim.api.nvim_set_keymap('n', '<Leader>h', '<CMD>Telescope oldfiles<CR>', opts_ns)
 vim.api.nvim_set_keymap('n', '<Leader>s', '<CMD>Telescope current_buffer_fuzzy_find<CR>', opts_ns)
 vim.api.nvim_set_keymap('n', '<Leader>a', '<CMD>lua require("fzf-lua").grep()<CR>', opts_n)
-vim.api.nvim_set_keymap('n', '<space>ca', '<CMD>Telescope lsp_code_actions<CR>', opts_ns)
+vim.api.nvim_set_keymap('n', '<space>ca', '<CMD>lua vim.lsp.buf.code_action()<CR>', opts_ns)
 vim.api.nvim_set_keymap('v', '<space>ca', '<CMD>Telescope lsp_range_code_actions<CR>', opts_ns)
 vim.api.nvim_set_keymap('n', '<Leader>ld', '<CMD>Telescope diagnostics<CR>', opts_ns)
 vim.api.nvim_set_keymap('n', 'gr', "<CMD>lua require('telescope.builtin').lsp_references{fname_width = 86}<CR>", opts_ns)
